80e912d8346305c2b4b1f660397061395459047d
TheDeagle <castlehaitham@gmail.com>
2024-11-15 02:14:22 +0100

    push

--- ft_write/ft_write.s
+++ ft_write/ft_write.s
@@ -1,26 +1,29 @@
 section .data
-	msg db "Hello world!", 10 
+	msg db "Hello world!", 0
 
 section .text
 	global _start
 
+
 _start:
-	call main
-	
-	mov rax, 0x200001
+	call _main
+
+	mov rax, 0x2000001
 	mov rdi, 0
 	syscall
 
-main:
-	mov rsi, msg
-	mov rdx, 13 
-	call ft_write
-
+ft_write:
+	mov rax, 0x2000004
+	mov rdi, 1
+	mov rsi, [rbp + 8]
+	mov rdx, 12
+	syscall
 	ret
 
-ft_write:
-	mov rax, 0x200004
-	mov rdi, 1
-	syscall	
+_main:
+	push msg
+	push rbp
+	mov rbp, rsp
+	call ft_write
 
 	ret
