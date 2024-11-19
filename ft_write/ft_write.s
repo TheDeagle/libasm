06e3ba5934a112c310967673c641d448b4d02acc
TheDeagle <TheDeagle@somewher.com>
2024-11-19 01:19:32 +0000

    ft_write

--- ft_write/ft_write.s
+++ ft_write/ft_write.s
@@ -1,11 +1,11 @@
 section .text
 	global ft_write
+	extern __errno_location
 
 ft_write:
 	mov rax, 0x01
 	syscall
 	
-	mov rbx, rax
 	test rax, rax
 	js _error
 	
@@ -13,6 +13,11 @@ ft_write:
 
 _error:
 	neg rax
+	mov rbx, rax
+	call __errno_location
+	mov [rax], rbx
+	mov rax, -1	
+
 	ret
 	
 
